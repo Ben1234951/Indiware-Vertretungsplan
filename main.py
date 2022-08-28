0d6c98b4eb0c917574584c826c5eabdde6469bf0
Ben1234951 <102352012+Ben1234951@users.noreply.github.com>
2022-08-28 16:50:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+import plan 
+import discord
+from discord.ext import commands, tasks
+from datetime import datetime
+from threading import Thread
+
+def Discord():
+    Discord_client = commands.Bot(command_prefix='!')
+
+    @Discord_client.event
+    async def on_ready():
+        loop.start()
+        print(f"{Discord_client.user} has connected")
+
+    @Discord_client.command(aliases=["plan", "vertretungsplan", "Vertretungsplan"], pass_context=True)
+    async def Plan(ctx, Klasse : discord.Member=None):
+        if Klasse is None:
+            with open(f"Klassen\\{ctx.author.id}", "r") as f:
+                Klasse = f.read()
+        data = plan.get_data(Klasse)
+        embed=discord.Embed(title="Dein Vertretungsplan ist: ", description=f"{data}", color=0x00ff00)
+        await ctx.send(embed=embed)
+
+    @Discord_client.command()
+    async def add_class(ctx, Klasse : str):
+        with open(f"Klassen\\{ctx.author.id}", "w") as f:
+            f.write(Klasse)
+        embed = discord.Embed(title=f"Du wurdest zur Klasse {Klasse} hinzugefügt", color=0x00ff00)
+        await ctx.send(embed=embed)
+
+    @tasks.loop(minutes=1)
+    async def loop():
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        if "18:00" | "07:30" in current_time:
+            channel = Discord_client.get_channel(1013376915949760562)
+            data_9_5 = plan.get_data(9.5)
+            data_8_5 = plan.get_data(8.5)
+            embed2 = discord.Embed(title="Vertretungsplan der Klasse 8.5", description=f"{data_8_5}", color=0x00ff00)
+            embed1 = discord.Embed(title="Vertretungsplan der Klasse 9.5", description=f"{data_9_5}", color=0x00ff00)
+            await channel.send(embed=embed2)
+            await channel.send(embed=embed1)
+
+    Discord_client.run("MTAxMzM3NTc3NTYxNDk4MDEyNw.GE5a5E.41WnEoYeUscEr-SSO4NqRa9NHKGRCYYe5IH2Zs")
+
+if __name__ == "__main__":
+    Discord()
---
+++ plan.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def get_data(Klasse : str):
+    options = Options()
+    options.headless = True
+
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get("https://schueler:sm37721@www.stundenplan24.de/10019573/mobil")
+    driver.get("https://www.stundenplan24.de/10019573/mobil/index.html")
+
+    driver.execute_script(f"SeiteAKPlanAufrufen('{Klasse}')")
+
+    html_list = driver.find_element_by_id("plan")
+    list = html_list.find_elements_by_tag_name("li")
+    
+    maintem = ""
+    for item in list:
+        # print(item.text)
+        maintem += item.text + "\n"
+    driver.quit()
+    return maintem
